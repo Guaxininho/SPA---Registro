505f463c325ff386561d3c68ee4753850957de3b
Kiiroi <sammydevjs@gmail.com>
2023-01-15 11:06:53 -0300

    basico da navegacao concluido

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./reset.scss";
+import "./navegacao.scss";
+import "./fonts.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navegacao />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// VOU CRIAR TUDO AQUI E DEPOIS SEPARAR COMO COMPONENTES
+function Navegacao() {
+  return (
+    <nav>
+      <img src="logo.png" alt="logo" />
+      <h1>Registro</h1>
+      <div className="flexDeNavegacao">
+        <button>Documentação</button>
+        <button>Ferramentas</button>
+        <button>Certificados</button>
+        <button>Portfólio</button>
+        <button>Roadmap</button>
+        <button>Contato</button>
+      </div>
+    </nav>
+  );
+}
